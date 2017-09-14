ac70080739104167b6e5706458d2b6cded6d2f55
Chad Mills <chadlmills@gmail.com>
2017-09-14 23:34:19 +0000

    basic functionality in place

--- controller.py
+++ controller.py
@@ -14,86 +14,66 @@ config_path = os.getcwd() + "/config.yaml"
 with open(config_path, 'r') as stream:
     config = yaml.safe_load(stream)
 
-for door in config['doors']:
-    config['doors'][door]['last_action'] = None
-    config['doors'][door]['last_action_time'] = None
-    config['doors'][door]['current_status'] = None
+config['door']['last_action'] = None
+config['door']['last_action_time'] = None
+config['door']['current_status'] = None
 
+gpio.setwarnings(False)
+gpio.cleanup()
 gpio.setmode(gpio.BCM)
-gpio.setup()
+gpio.setup(config['door']['relay_pin'], gpio.OUT)
+gpio.setup(config['door']['state_pin'], gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.output(config['door']['relay_pin'], True)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
     user = config['config']['user']
-    name = config['doors']['only']['name']
-    return render_template('index.html', user=user, name=name)
+    name = config['door']['name']
+    status = get_door_status()
+    cdate= 'RIGHT NOW!' #change this
+    return render_template('index.html', user=user, name=name, status=status, cdate=cdate)
 
-
-def uptime(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separator=' '):
-    """
-    Takes an amount of seconds and turns it into a human-readable amount of time.
-    """
-    # the formatted time string to be returned
-    time = []
-
-    # the pieces of time to iterate over (days, hours, minutes, etc)
-    # - the first piece in each tuple is the suffix (d, h, w)
-    # - the second piece is the length in seconds (a day is 60s * 60m * 24h)
-    parts = [(suffixes[0], 60 * 60 * 24 * 7 * 52),
-             (suffixes[1], 60 * 60 * 24 * 7),
-             (suffixes[2], 60 * 60 * 24),
-             (suffixes[3], 60 * 60),
-             (suffixes[4], 60),
-             (suffixes[5], 1)]
-
-    # for each time piece, grab the value and remaining seconds, and add it to
-    # the time string
-    for suffix, length in parts:
-        value = seconds / length
-        if value > 0:
-            seconds = seconds % length
-            time.append('%s%s' % (str(value),
-                                  (suffix, (suffix, suffix + 's')[value > 1])[add_s]))
-        if seconds < 1:
-            break
-
-    return separator.join(time)
-
-def get_door_status(door):
-    if gpio.input(config['doors'][door]['state_pin']) == config['doors'][door]['state_pin_closed']
+@app.route('/status')
+def get_door_status():
+    if gpio.input(config['door']['state_pin']) == config['door']['state_pin_closed_value']:
         return 'closed'
-    elif config['doors'][door]['last_action'] == 'open':
-        if time.time() - config['doors'][door]['last_action_time] >= config['doors'][door].get('approx_time_to_open', 10):
+    elif config['door']['last_action'] == 'open':
+        if time.time() - config['door']['last_action_time'] >= config['door'].get('approx_time_to_open', 10):
             return 'open'
         else:
             return 'opening'
-    elif self.last_action == 'close'
-        if time.time() - config['doors'][door]['last_action_time] >= config['doors'][door].get('approx_time_to_close', 10):
+    elif config['door']['last_action'] == 'close':
+        if time.time() - config['door']['last_action_time'] >= config['door'].get('approx_time_to_close', 10):
             return 'open' #This state indicates a problem
         else:
             return 'closing'
     else:
         return 'open'
 
-def toggle_relay(door):
-    state = get_door_status(door)
+@app.route('/toggle')
+def toggle_relay():
+    state = get_door_status()
     if (state == 'open'):
-        config['doors'][door]['last'action] = 'close'
-        config['doors'][door]['last_action_time'] = time.time()
-    if (state == 'closed'):
-        config['doors'][door]['last'action] = 'open'
-        config['doors'][door]['last_action_time'] = time.time()
+        config['door']['last_action'] = 'close'
+        config['door']['last_action_time'] = time.time()
+    elif (state == 'closed'):
+        config['door']['last_action'] = 'open'
+        config['door']['last_action_time'] = time.time()
     else:
-        config['doors'][door]['last'action] = None
-        config['doors'][door]['last_action_time'] = None
+        config['door']['last_action'] = None
+        config['door']['last_action_time'] = None
 
-    gpio.output(config['doors'][door]['relay_pin'], False)
+    gpio.output(config['door']['relay_pin'], False)
     time.sleep(0.2)
-    gpio.output(config['doors'][door]['relay_pin'], True)
+    gpio.output(config['door']['relay_pin'], True)
+    if config['door']['last_action'] == 'close':
+        return 'closing'
+    if config['door']['last_action'] == 'open':
+        return 'opening'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=config['site']['port'], host=config['site']['access'])
 
